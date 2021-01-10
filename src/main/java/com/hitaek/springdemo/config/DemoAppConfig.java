b956b4110f1d0101e36b190ff71323d3c1ed5a5b
leegyotaek <leegyotaek@gmail.com>
2021-01-10 16:40:16 +0900

    add sample rest controller

---
+++ src/main/java/com/hitaek/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,13 @@
+package com.hitaek.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.hitaek.springdemo")
+public class DemoAppConfig implements WebMvcConfigurer {
+
+}
